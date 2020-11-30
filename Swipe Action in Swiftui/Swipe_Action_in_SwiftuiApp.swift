2eb89af90ce486c3adccc2479a680ad018371949
hosam mohamed <hosamelmalt@gmail.com>
2020-11-30 13:23:18 -0800

    Initial Commit

---
+++ Swipe Action in Swiftui/Swipe_Action_in_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_Action_in_SwiftuiApp.swift
+//  Swipe Action in Swiftui
+//
+//  Created by Hossam on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_Action_in_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
